df9a4298601161f3668c8b67316b570eaf89098d
HangerThem <f.borisjuk@hangerthem.com>
2024-11-07 13:21:48 +0100

    005_min_a_max added

---
+++ IS-Projects/program_004_pseudorandom_number_generator/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace program_004_pseudorandom_number_generator {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Enter n, top and bottom:");
+			int n = int.Parse(Console.ReadLine());
+			int top = int.Parse(Console.ReadLine());
+			int bottom = int.Parse(Console.ReadLine());
+			
+			int[] numbers = new int[n];
+			Random random = new();
+
+			int negative = 0;
+			int positive = 0;
+			int zero = 0;
+			int odd = 0;
+			int even = 0;
+
+			for (int i = 0; i < n; i++) {
+				numbers[i] = random.Next(bottom, top);
+				if (numbers[i] < 0) {
+					negative++;
+				} else if (numbers[i] > 0) {
+					positive++;
+				} else {
+					zero++;
+				}
+
+				if (numbers[i] % 2 == 0) {
+					even++;
+				} else {
+					odd++;
+				}
+			}
+
+			Console.WriteLine("Negative: " + negative);
+			Console.WriteLine("Positive: " + positive);
+			Console.WriteLine("Zero: " + zero);
+			Console.WriteLine("Odd: " + odd);
+			Console.WriteLine("Even: " + even);
+		}
+	}
+}
---
+++ IS-Projects/program_005_max_a_min/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace program_005_min_a_max {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Enter n, top and bottom:");
+			int n = int.Parse(Console.ReadLine());
+			int top = int.Parse(Console.ReadLine());
+			int bottom = int.Parse(Console.ReadLine());
+			
+			int[] numbers = new int[n];
+			Random random = new();
+
+			for (int i = 0; i < n; i++) {
+				numbers[i] = random.Next(bottom, top);
+			}
+
+			int max = numbers[0];
+			List<int> maxIndexes = [];
+			int min = numbers[0];
+			List<int> minIndexes = [];
+
+			for (int i = 1; i < n; i++) {
+				if (numbers[i] > max) {
+					max = numbers[i];
+					maxIndexes.Clear();
+					maxIndexes.Add(i);
+				} else if (numbers[i] == max) {
+					maxIndexes.Add(i);
+				}
+
+				if (numbers[i] < min) {
+					min = numbers[i];
+					minIndexes.Clear();
+					minIndexes.Add(i);
+				} else if (numbers[i] == min) {
+					minIndexes.Add(i);
+				}
+			}
+
+			
+
+			Console.WriteLine("Numbers:");
+			foreach (int number in numbers) {
+				Console.Write(number + " ");
+			}
+			Console.WriteLine();
+			Console.WriteLine("Max: " + max);
+			Console.WriteLine("Max index(es):");
+			foreach (int index in maxIndexes) {
+				Console.Write(index + " ");
+			}
+			Console.WriteLine();
+			Console.WriteLine("Min: " + min);
+			Console.WriteLine("Min index(es):");
+			foreach (int index in minIndexes) {
+				Console.Write(index + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
